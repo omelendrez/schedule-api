775bd0a1474db065fde96c3c450fdb5d4e7ecaf9
Omar Melendrez <omar.melendrez@gmail.com>
2022-08-26 23:01:30 -0300

    Remove tracked files

--- db/backup.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set year=%DATE:~6,4%
-set month=%DATE:~3,2%
-set day=%DATE:~0,2%
-set hour=%TIME:~0,2%
-set minute=%TIME:~3,2%
-set backuptime=%year%%month%%day%%hour%%minute%
-"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysqldump" -u root heroku_43e6ebe3dd474ec --databases --column-statistics=0 -h localhost --password=M1a4$1t4E8r0 > "%backuptime%".sql
-exit
