459515d16d1def8ca13a37fd0a26ad667249b01a
Omar Melendrez <omar.melendrez@gmail.com>
2022-08-28 15:18:14 -0300

    Fix token duration

--- api/v1/middleware/auth.js
+++ api/v1/middleware/auth.js
@@ -7,23 +7,23 @@ module.exports = {
     if (authorizationHeaader) {
       const token = req.headers.authorization.split(' ')[1];
       try {
-        result = jwt.verify(token, process.env.JWT_SECRET, { expiresIn: '1d' });
+        result = jwt.verify(token, process.env.JWT_SECRET);
         req.decoded = result
         next();
       } catch (err) {
-        return res.status(401).send({
-          code: 401,
-          message: 'Token expirada',
-          detail: 'La token usada para acceder a la base de datos ha expirado. Por favor a loguearse.'
-        });
+        if (err.message === 'jwt expired') {
+          result = { code: 401, message: 'Token expirada', detail: 'La token necesaria para acceder a la base de datos ha expirado, por favor volver a loguearse.' }
+        } else {
+          result = { code: 401, message: 'Token requerida', detail: 'Una token es requerida para acceder a este recurso.' }
+        }
+        return res.status(401).send(result);
       }
     } else {
       result = {
-        code: 401,
-        message: 'Token requerida',
-        detail: 'Una token es requerida para acceder a este recurso.'
-      }
-    };
-    return res.status(401).send(result);
+        error: `Authentication error. Token required.`,
+        status: 401
+      };
+      return res.status(401).send(result);
+    }
   }
 };
